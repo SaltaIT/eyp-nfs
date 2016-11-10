47f762235bfaa0fbd978502a2cc1c44d1ef5999d
Jordi Prats <jprats@systemadmin.es>
2016-11-10 17:59:06 +0100

    lint

--- manifests/install.pp
+++ manifests/install.pp
@@ -7,7 +7,7 @@ class nfs::install inherits nfs {
   if($nfs::manage_package)
   {
     package { $nfs::params::package_name:
-     ensure => $nfs::package_ensure,
+      ensure => $nfs::package_ensure,
     }
   }
 
