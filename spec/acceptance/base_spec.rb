b354067471a47b6782355cebcef3934ab69b362d
Jordi Prats <jordi.prats@gmail.com>
2018-10-31 17:59:26 +0100

    export tmp

--- spec/acceptance/base_spec.rb
+++ spec/acceptance/base_spec.rb
@@ -26,14 +26,14 @@ describe 'nfs class' do
         is_server => true,
       }
 
-      nfs::export { '/etc':
+      nfs::export { '/tmp':
         fsid => '1',
       }
 
       ->
 
-      nfs::nfsmount { '/mnt/etc':
-        nfsdevice => '127.0.0.1:/etc',
+      nfs::nfsmount { '/mnt/tmp':
+        nfsdevice => '127.0.0.1:/tmp',
         require   => Class['nfs::service'],
       }
 
